7a94ee02848ea5dc5a0dfa147441b1ba07dbe4dc
Joshua Wong <dsjw2@cam.ac.uk>
2017-06-16 01:20:52 +0100

    Update UniversalRM.java

--- UniversalRM.java
+++ UniversalRM.java
@@ -61,15 +61,11 @@ public class UniversalRM {
 				//decode as <<2i+1, <j,k> >>
 				Pair innerPair = decodePair(pair.y,1);
 				pair.x--;
-				System.out.println(pair.x);
-				System.out.println(innerPair.x);
-				System.out.println(innerPair.y);
-				
 				minus(pair.x/2, innerPair.x, innerPair.y);
-				
 			}
 		}
 	}
+	
 	private void add(int i, int j) {
 		//add 1 to reg i and run instruction j
 		System.out.println("L"+pc+" : R"+i+"+ --> L"+j);
@@ -79,6 +75,7 @@ public class UniversalRM {
 		if (j>=prog.size()) halt(1);
 		else decode(prog.get(j));
 	}
+	
 	private void minus(int i, int j, int k) {
 		//if greater then 0, subtract and goto j, else k
 		System.out.println("L"+pc+" : R"+i+"- --> L"+j+",L"+k);
@@ -93,21 +90,24 @@ public class UniversalRM {
 			else decode(prog.get(k));
 		}
 	}
+	
 	private void pad(int n) {
 		while (regs.size()<n) regs.add(0);
 	}
+	
 	private void halt() {
 		pc++;
 		System.out.println("HALT");
 		System.out.println(regs);
 	}
+	
 	private void halt(int x) {
 		System.out.println("ERRONEOUS HALT");
 		System.out.println(regs);
 	}
+	
 	public void compute() {
 		if (prog.isEmpty()) decode(0);
 		else decode(prog.get(0));
 	}
-
 }
